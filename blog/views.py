180f24a80a8750e1929ea901698b8247c70fa31f
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-01-31 14:01:21 +0100

    Starting to display articles

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from blog.models import Article
 
 def index(request):
-    context = {}
+    latest_blog_posts = Article.objects.order_by('-date')[:10]
+    context = {'latest_blog_posts': latest_blog_posts}
     return render(request, 'blog/index.html', context)
