66c7cf37e64176f78049a87575e152efd989765c
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-01-31 13:53:07 +0100

    Adding Article model

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    date = models.DateTimeField('date published')
+    draft = models.BooleanField()
+    def __str__(self):              # __unicode__ on Python 2
+        return self.title
