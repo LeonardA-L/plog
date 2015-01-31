13c00aa58bea8bef5484ff052ae61854ba8d146d
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-01-31 14:20:34 +0100

    Do not display drafts

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from blog.models import Article
 
 def index(request):
-    latest_blog_posts = Article.objects.filter(date__lte = datetime.today())
+    latest_blog_posts = Article.objects.filter(date__lte = datetime.today())	# Do not display posts that are to be published later
+    latest_blog_posts = latest_blog_posts.filter(draft = False)
     latest_blog_posts = latest_blog_posts.order_by('-date')[:10]
     context = {'latest_blog_posts': latest_blog_posts}
     return render(request, 'blog/index.html', context)
