f74793be8c4f6408f781fd077d73a86626f0d8aa
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-02-03 16:50:25 +0100

    Starting to write tests for Article

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from datetime import datetime, timedelta
+from django.utils.dateparse import parse_date
+
+from blog.models import Article
+from blog.tools import *
 
 # Create your tests here.
+
+class ArticleTests(TestCase):
+	def test_future_article_will_be_public(self):
+		p = Article(title='test', content='test', draft=False, date = datetime.today() + timedelta(days=30), commentable=True)
+		p.save()
+		lbp = retrieveArticles(limit=-1, drafts=False, future=False)
+		contained = p in lbp
+		p.delete()
+		self.assertEqual(contained, False)
+
+	def test_draft_article_will_be_public(self):
+		p = Article(title='test', content='test', draft=True, date = datetime.today() - timedelta(days=30), commentable=True)
+		p.save()
+		lbp = retrieveArticles(limit=-1, drafts=False, future=False)
+		contained = p in lbp
+		p.delete()
+		self.assertEqual(contained, False)
