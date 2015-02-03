1da898f45205f247280772a52e754ec6fe7dd3e0
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-02-03 09:15:26 +0100

    Added a filter that strips JS out of content but keeps HTML formatting

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,8 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+from django.utils.encoding import force_unicode
+import re
 
 register = template.Library()
 
@@ -17,6 +21,15 @@ def div(value, arg):
     return int(value) / int(arg)
 div.is_safe = False
 
+# The purpose of this filter (found online) is to strip JS tags and keep HTML
+# I don't know if it's enough to sanitize the inputs though
+# Hence, the comment section won't get labeled as safe since it's a public part
+@stringfilter
+def stripjs(value):
+    stripped = re.sub(r'<script(?:\s[^>]*)?(>(?:.(?!/script>))*</script>|/>)', '', force_unicode(value), flags=re.S)
+    return mark_safe(stripped)
+
 register.filter('mult', mult)
 register.filter('sub', sub)
 register.filter('div', div)
+register.filter('stripjs',stripjs)
